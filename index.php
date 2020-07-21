8e21bca5f8a7d298654ce1a748d2ccaace932975
ali awais <ali.awais5044@gmail.com>
2020-07-21 16:32:07 +0400

    changed path

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 $root_path = '/var/www/multibankfx';
-include $root_path.'helper/header_include.php';
+include $root_path.'/var/www/multibankfx/helper/header_include.php';
 
 include $root_path.$request_file;
