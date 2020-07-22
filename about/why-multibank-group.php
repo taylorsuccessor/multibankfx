ceccf207735e6dc052e3b23671da5000bb88c4e1
ali awais <ali.awais5044@gmail.com>
2020-07-22 13:24:18 +0400

    iii

--- about/why-multibank-group.php
+++ about/why-multibank-group.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang='en'>
-    <?php include_from_root('website_template/head.php');?>
+    <?php include_from_root('/website_template/head.php');?>
 <body>
-    <?php include_from_root('website_template/header_and_menu.php');?>
+    <?php include_from_root('/website_template/header_and_menu.php');?>
 <style>
     .nav-menu-hamburger, .nav-menu-mymex {
         width: 35px;
