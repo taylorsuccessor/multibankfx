e6bc0208773b057c8d124ef53d333a801b7c6855
ali awais <ali.awais5044@gmail.com>
2020-07-22 12:20:38 +0400

    iii

--- website_template/head.php
+++ website_template/head.php
@@ -1,14 +1,3 @@
-<?php
-$root_path = __DIR__."/";
-include $root_path.'../helper/header_include.php';
-
-function include_from_root($path)
-    {
-        include($root_path.$path);
-    }
-
-include $root_path.$request_file;
-?>
 <head>
     <meta charset='UTF-8'>
     <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
