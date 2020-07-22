ae3993ffede1ecab14887d921d4f85f7e180156c
ali awais <ali.awais5044@gmail.com>
2020-07-22 11:20:41 +0400

    iii

--- about/why-multibank-group.php
+++ about/why-multibank-group.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang='en'>
-    <?php include_from_root('website_template/head.php');?>
+   
 <body>
     <?php include_from_root('website_template/header_and_menu.php');?>
 <style>
